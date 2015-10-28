e79c8f1e61275d45ce8dc8d90652e985f5e52018
szenia <s.zadvornykh@gmail.com>
2015-10-27 23:41:48 -0400

    started interactive example

---
+++ examples/interactive/main.js
@@ -0,0 +1,268 @@
+var mContainer;
+var mCamera, mRenderer;
+var mControls;
+
+var mScene;
+
+var mParticleCount = 50000; // <-- change this number!
+var mParticleSystem;
+
+var mTime = 0.0;
+var mTimeStep = (1/60);
+var mDuration = 20;
+
+window.onload = function () {
+  init();
+};
+
+function init() {
+  initTHREE();
+  initControls();
+  initParticleSystem();
+
+  requestAnimationFrame(tick);
+  window.addEventListener('resize', resize, false);
+}
+
+function initTHREE() {
+  mRenderer = new THREE.WebGLRenderer({antialias: false});
+  mRenderer.setSize(window.innerWidth, window.innerHeight);
+
+  mContainer = document.getElementById('three-container');
+  mContainer.appendChild(mRenderer.domElement);
+
+  mCamera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 10000);
+  mCamera.position.set(0, 600, 600);
+
+  mScene = new THREE.Scene();
+
+  mScene.add(new THREE.GridHelper(2000, 100));
+
+  var light;
+
+  light = new THREE.PointLight(0xffffff, 1, 0, 2);
+  light.position.set(0, 400, 0);
+  mScene.add(light);
+}
+
+function initControls() {
+  mControls = new THREE.OrbitControls(mCamera, mRenderer.domElement);
+}
+
+function initParticleSystem() {
+  var prefabGeometry = new THREE.DodecahedronGeometry(4);
+  var bufferGeometry = new THREE.BAS.PrefabBufferGeometry(prefabGeometry, mParticleCount);
+
+  bufferGeometry.computeVertexNormals();
+
+  // generate additional geometry data
+  var aOffset = bufferGeometry.createAttribute('aOffset', 1);
+  var aStartPosition = bufferGeometry.createAttribute('aStartPosition', 3);
+  var aControlPoint1 = bufferGeometry.createAttribute('aControlPoint1', 3);
+  var aControlPoint2 = bufferGeometry.createAttribute('aControlPoint2', 3);
+  var aEndPosition = bufferGeometry.createAttribute('aEndPosition', 3);
+  var aAxisAngle = bufferGeometry.createAttribute('aAxisAngle', 4);
+  var aColor = bufferGeometry.createAttribute('color', 3);
+
+  var i, j, offset;
+
+  // buffer time offset
+  var delay;
+
+  for (i = 0, offset = 0; i < mParticleCount; i++) {
+    delay = i / mParticleCount * mDuration;
+
+    for (j = 0; j < prefabGeometry.vertices.length; j++) {
+      aOffset.array[offset++] = delay;
+    }
+  }
+
+  // buffer start positions
+  var x, y, z;
+
+  for (i = 0, offset = 0; i < mParticleCount; i++) {
+    x = -1000;
+    y = 0;
+    z = 0;
+
+    for (j = 0; j < prefabGeometry.vertices.length; j++) {
+      aStartPosition.array[offset++] = x;
+      aStartPosition.array[offset++] = y;
+      aStartPosition.array[offset++] = z;
+    }
+  }
+
+  // buffer control points
+
+  for (i = 0, offset = 0; i < mParticleCount; i++) {
+    x = THREE.Math.randFloat(-400, 400);
+    y = THREE.Math.randFloat(-400, 400);
+    z = THREE.Math.randFloat(-1200, -800);
+
+    for (j = 0; j < prefabGeometry.vertices.length; j++) {
+      aControlPoint1.array[offset++] = x;
+      aControlPoint1.array[offset++] = y;
+      aControlPoint1.array[offset++] = z;
+    }
+  }
+
+  for (i = 0, offset = 0; i < mParticleCount; i++) {
+    x = THREE.Math.randFloat(-400, 400);
+    y = THREE.Math.randFloat(-400, 400);
+    z = THREE.Math.randFloat(800, 1200);
+
+    for (j = 0; j < prefabGeometry.vertices.length; j++) {
+      aControlPoint2.array[offset++] = x;
+      aControlPoint2.array[offset++] = y;
+      aControlPoint2.array[offset++] = z;
+    }
+  }
+
+  // buffer end positions
+
+  for (i = 0, offset = 0; i < mParticleCount; i++) {
+    x = 1000;
+    y = 0;
+    z = 0;
+
+    for (j = 0; j < prefabGeometry.vertices.length; j++) {
+      aEndPosition.array[offset++] = x;
+      aEndPosition.array[offset++] = y;
+      aEndPosition.array[offset++] = z;
+    }
+  }
+
+  // buffer axis angle
+  var axis = new THREE.Vector3();
+  var angle = 0;
+
+  for (i = 0, offset = 0; i < mParticleCount; i++) {
+    axis.x = THREE.Math.randFloatSpread(2);
+    axis.y = THREE.Math.randFloatSpread(2);
+    axis.z = THREE.Math.randFloatSpread(2);
+    axis.normalize();
+
+    angle = Math.PI * THREE.Math.randInt(16, 32);
+
+    for (j = 0; j < prefabGeometry.vertices.length; j++) {
+      aAxisAngle.array[offset++] = axis.x;
+      aAxisAngle.array[offset++] = axis.y;
+      aAxisAngle.array[offset++] = axis.z;
+      aAxisAngle.array[offset++] = angle;
+    }
+  }
+
+  // buffer color
+  var color = new THREE.Color();
+  var h, s, l;
+
+  for (i = 0, offset = 0; i < mParticleCount; i++) {
+    h = i / mParticleCount;
+    s = THREE.Math.randFloat(0.4, 0.6);
+    l = THREE.Math.randFloat(0.4, 0.6);
+
+    color.setHSL(h, s, l);
+
+    for (j = 0; j < prefabGeometry.vertices.length; j++) {
+      aColor.array[offset++] = color.r;
+      aColor.array[offset++] = color.g;
+      aColor.array[offset++] = color.b;
+    }
+  }
+
+
+  var material = new THREE.BAS.PhongAnimationMaterial(
+    // custom parameters & THREE.MeshPhongMaterial parameters
+    {
+      vertexColors: THREE.VertexColors,
+      shading: THREE.FlatShading,
+      side: THREE.DoubleSide,
+      uniforms: {
+        uTime: {type: 'f', value: 0},
+        uDuration: {type: 'f', value: mDuration},
+        uPoint: {type:'v3', value:new THREE.Vector3(200)}
+      },
+      shaderFunctions: [
+        THREE.BAS.ShaderChunk['quaternion_rotation'],
+        THREE.BAS.ShaderChunk['cubic_bezier']
+      ],
+      shaderParameters: [
+        'uniform float uTime;',
+        'uniform float uDuration;',
+        'uniform vec3 uPoint;',
+        'attribute float aOffset;',
+        'attribute vec3 aStartPosition;',
+        'attribute vec3 aControlPoint1;',
+        'attribute vec3 aControlPoint2;',
+        'attribute vec3 aEndPosition;',
+        'attribute vec4 aAxisAngle;'
+      ],
+      shaderVertexInit: [
+        'float tProgress = mod((uTime + aOffset), uDuration) / uDuration;',
+
+        'float angle = aAxisAngle.w * tProgress;',
+        'vec4 tQuat = quatFromAxisAngle(aAxisAngle.xyz, angle);'
+      ],
+      shaderTransformNormal: [
+        'objectNormal = rotateVector(tQuat, objectNormal);'
+      ],
+      shaderTransformPosition: [
+        'transformed = rotateVector(tQuat, transformed);',
+        'transformed += cubicBezier(aStartPosition, aControlPoint1, aControlPoint2, aEndPosition, tProgress);',
+
+        'vec3 tDelta = transformed - uPoint;',
+        'float tDistance = length(tDelta);',
+        'float tForce = 1.0 - min(tDistance, 200.0) / 200.0;',
+        'tDelta *= tForce;',
+        'transformed += tDelta;'
+      ]
+    },
+    // THREE.MeshPhongMaterial uniforms
+    {
+      specular: 0xff0000,
+      shininess: 20
+    }
+  );
+
+  mParticleSystem = new THREE.Mesh(bufferGeometry, material);
+  // because the bounding box of the particle system does not reflect its on-screen size
+  // set this to false to prevent the whole thing from disappearing on certain angles
+  mParticleSystem.frustumCulled = false;
+
+  mScene.add(mParticleSystem);
+}
+
+var _x = 0;
+var _y = 0;
+
+function tick() {
+  update();
+  render();
+
+  mTime += mTimeStep;
+  mTime %= mDuration;
+
+  requestAnimationFrame(tick);
+}
+
+function update() {
+
+  _x = Math.cos(mTime) * 100;
+  _y = Math.sin(mTime) * 100;
+
+  mControls.update();
+  mParticleSystem.material.uniforms['uPoint'].value.x = _x;
+  mParticleSystem.material.uniforms['uPoint'].value.z = _y;
+  mParticleSystem.material.uniforms['uTime'].value = mTime;
+}
+
+function render() {
+  mRenderer.render(mScene, mCamera);
+}
+
+function resize() {
+  mCamera.aspect = window.innerWidth / window.innerHeight;
+  mCamera.updateProjectionMatrix();
+
+  mRenderer.setSize(window.innerWidth, window.innerHeight);
+}
