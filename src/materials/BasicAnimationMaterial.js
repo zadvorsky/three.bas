f1223320d2af3d71e6dc8375346d35f694da44fb
Szenia Zadvornykh <s.zadvornykh@gmail.com>
2016-03-15 16:06:20 -0400

    adds BasicAnimationMaterial

---
+++ src/materials/BasicAnimationMaterial.js
@@ -0,0 +1,73 @@
+THREE.BAS.BasicAnimationMaterial = function(parameters, uniformValues) {
+  THREE.BAS.BaseAnimationMaterial.call(this, parameters);
+
+  var basicShader = THREE.ShaderLib['basic'];
+
+  this.uniforms = THREE.UniformsUtils.merge([basicShader.uniforms, this.uniforms]);
+  this.lights = false;
+  this.vertexShader = this._concatVertexShader();
+  this.fragmentShader = basicShader.fragmentShader;
+
+  // todo add missing default defines
+  uniformValues.map && (this.defines['USE_MAP'] = '');
+  uniformValues.normalMap && (this.defines['USE_NORMALMAP'] = '');
+
+  this.setUniformValues(uniformValues);
+};
+THREE.BAS.BasicAnimationMaterial.prototype = Object.create(THREE.BAS.BaseAnimationMaterial.prototype);
+THREE.BAS.BasicAnimationMaterial.prototype.constructor = THREE.BAS.BasicAnimationMaterial;
+
+THREE.BAS.BasicAnimationMaterial.prototype._concatVertexShader = function() {
+  // based on THREE.ShaderLib.phong
+  return [
+
+    THREE.ShaderChunk[ "common" ],
+    THREE.ShaderChunk[ "uv_pars_vertex" ],
+    THREE.ShaderChunk[ "uv2_pars_vertex" ],
+    THREE.ShaderChunk[ "envmap_pars_vertex" ],
+    THREE.ShaderChunk[ "color_pars_vertex" ],
+    THREE.ShaderChunk[ "morphtarget_pars_vertex" ],
+    THREE.ShaderChunk[ "skinning_pars_vertex" ],
+    THREE.ShaderChunk[ "logdepthbuf_pars_vertex" ],
+
+    this._concatFunctions(),
+
+    this._concatParameters(),
+
+    "void main() {",
+
+    this._concatVertexInit(),
+
+    THREE.ShaderChunk[ "uv_vertex" ],
+    THREE.ShaderChunk[ "uv2_vertex" ],
+    THREE.ShaderChunk[ "color_vertex" ],
+    THREE.ShaderChunk[ "skinbase_vertex" ],
+
+    "	#ifdef USE_ENVMAP",
+
+    THREE.ShaderChunk[ "beginnormal_vertex" ],
+
+    this._concatTransformNormal(),
+
+    THREE.ShaderChunk[ "morphnormal_vertex" ],
+    THREE.ShaderChunk[ "skinnormal_vertex" ],
+    THREE.ShaderChunk[ "defaultnormal_vertex" ],
+
+    "	#endif",
+
+    THREE.ShaderChunk[ "begin_vertex" ],
+
+    this._concatTransformPosition(),
+
+    THREE.ShaderChunk[ "morphtarget_vertex" ],
+    THREE.ShaderChunk[ "skinning_vertex" ],
+    THREE.ShaderChunk[ "project_vertex" ],
+    THREE.ShaderChunk[ "logdepthbuf_vertex" ],
+
+    THREE.ShaderChunk[ "worldpos_vertex" ],
+    THREE.ShaderChunk[ "envmap_vertex" ],
+
+    "}"
+
+  ].join( "\n" );
+};
