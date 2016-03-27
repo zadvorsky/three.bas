4b19f7903a726e2e3e4369a782b2716398539dce
szenia <s.zadvornykh@gmail.com>
2016-03-27 16:26:23 -0400

    refactoring

--- examples/letters/main.js
+++ examples/letters/main.js
@@ -1,45 +1,21 @@
-var mContainer;
-var mCamera, mRenderer;
-var mControls;
-var mScene;
-
-window.onload = function () {
-  init();
-};
+window.onload = init;
 
 function init() {
-  initTHREE();
-  // initControls();
+  var root = new THREERoot({
+    createCameraControls:false,
+    fov:10
+  });
+  root.renderer.setClearColor(0xffffff);
+  root.camera.position.set(0, 0, 1400);
 
   var textAnimation = createTextAnimation();
-  mScene.add(textAnimation);
+  root.scene.add(textAnimation);
 
   var tween = TweenMax.fromTo(textAnimation, 4,
     {animationProgress:0},
     {animationProgress:1, ease:Power1.easeInOut, repeat:-1, yoyo:true}
   );
   createTweenScrubber(tween);
-
-  requestAnimationFrame(tick);
-  window.addEventListener('resize', resize, false);
-}
-
-function initTHREE() {
-  mRenderer = new THREE.WebGLRenderer({antialias: true});
-  mRenderer.setSize(window.innerWidth, window.innerHeight);
-  mRenderer.setClearColor(0xffffff);
-
-  mContainer = document.getElementById('three-container');
-  mContainer.appendChild(mRenderer.domElement);
-
-  mCamera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 5000);
-  mCamera.position.set(0, 0, 1400);
-
-  mScene = new THREE.Scene();
-}
-
-function initControls() {
-  mControls = new THREE.OrbitControls(mCamera, mRenderer.domElement);
 }
 
 function createTweenScrubber(tween, seekSpeed) {
@@ -120,7 +96,87 @@ function createTextAnimation() {
 
   THREE.BAS.Utils.separateFaces(geometry);
 
-  var bufferGeometry = generateBufferGeometry(geometry);
+  return new TextAnimation(geometry);
+}
+
+function generateTextGeometry(text, params) {
+  var geometry = new THREE.TextGeometry(text, params);
+
+  geometry.computeBoundingBox();
+
+  geometry.userData = {};
+  geometry.userData.size = {
+    width: geometry.boundingBox.max.x - geometry.boundingBox.min.x,
+    height: geometry.boundingBox.max.y - geometry.boundingBox.min.y,
+    depth: geometry.boundingBox.max.z - geometry.boundingBox.min.z
+  };
+
+  var anchorX = geometry.userData.size.width * -params.anchor.x;
+  var anchorY = geometry.userData.size.height * -params.anchor.y;
+  var anchorZ = geometry.userData.size.depth * -params.anchor.z;
+  var matrix = new THREE.Matrix4().makeTranslation(anchorX, anchorY, anchorZ);
+
+  geometry.applyMatrix(matrix);
+
+  return geometry;
+}
+
+////////////////////
+// CLASSES
+////////////////////
+
+function TextAnimation(textGeometry) {
+  var bufferGeometry = new THREE.BAS.ModelBufferGeometry(textGeometry);
+
+  var aAnimation = bufferGeometry.createAttribute('aAnimation', 2);
+  var aControl0 = bufferGeometry.createAttribute('aControl0', 3);
+  var aControl1 = bufferGeometry.createAttribute('aControl1', 3);
+  var aEndPosition = bufferGeometry.createAttribute('aEndPosition', 3);
+
+  var faceCount = bufferGeometry.faceCount;
+  var i, i2, i3, i4, v;
+
+  var size = textGeometry.userData.size;
+  var length = new THREE.Vector3(size.width, size.height, size.depth).multiplyScalar(0.5).length();
+  var maxDelay = length * 0.06;
+
+  this.animationDuration = maxDelay + 4 + 1;
+  this._animationProgress = 0;
+
+  for (i = 0, i2 = 0, i3 = 0, i4 = 0; i < faceCount; i++, i2 += 6, i3 += 9, i4 += 12) {
+    var face = textGeometry.faces[i];
+    var centroid = THREE.BAS.Utils.computeCentroid(textGeometry, face);
+    var dirX = centroid.x > 0 ? 1 : -1;
+    var dirY = centroid.y > 0 ? 1 : -1;
+
+    // animation
+    var delay = centroid.length() * THREE.Math.randFloat(0.03, 0.06);
+    var duration = THREE.Math.randFloat(2, 4);
+
+    for (v = 0; v < 6; v += 2) {
+      aAnimation.array[i2 + v    ] = delay + Math.random();
+      aAnimation.array[i2 + v + 1] = duration;
+    }
+
+    // ctrl
+    var c0x = THREE.Math.randFloat(0, 30) * dirX;
+    var c0y = THREE.Math.randFloat(60, 120) * dirY;
+    var c0z = THREE.Math.randFloat(-20, 20);
+
+    var c1x = THREE.Math.randFloat(30, 60) * dirX;
+    var c1y = THREE.Math.randFloat(0, 60) * dirY;
+    var c1z = THREE.Math.randFloat(-20, 20);
+
+    for (v = 0; v < 9; v += 3) {
+      aControl0.array[i3 + v    ] = c0x;
+      aControl0.array[i3 + v + 1] = c0y;
+      aControl0.array[i3 + v + 2] = c0z;
+
+      aControl1.array[i3 + v    ] = c1x;
+      aControl1.array[i3 + v + 1] = c1y;
+      aControl1.array[i3 + v + 2] = c1z;
+    }
+  }
 
   var material = new THREE.BAS.BasicAnimationMaterial({
       shading: THREE.FlatShading,
@@ -156,112 +212,90 @@ function createTextAnimation() {
     }
   );
 
-  var mesh = new THREE.Mesh(bufferGeometry, material);
-  mesh.frustumCulled = false;
-  mesh._animationProgress = 0;
+  THREE.Mesh.call(this, bufferGeometry, material);
 
-  Object.defineProperty(mesh, 'animationProgress', {
-    get: function() {
-      return this._animationProgress;
-    },
-    set: function(v) {
-      this._animationProgress = v;
-      this.material.uniforms['uTime'].value = this.geometry.animationDuration * v;
-    }
+  this.frustumCulled = false;
+}
+TextAnimation.prototype = Object.create(THREE.Mesh.prototype);
+TextAnimation.prototype.constructor = TextAnimation;
+
+Object.defineProperty(TextAnimation.prototype, 'animationProgress', {
+  get: function() {
+    return this._animationProgress;
+  },
+  set: function(v) {
+    this._animationProgress = v;
+    this.material.uniforms['uTime'].value = this.animationDuration * v;
+  }
+});
+
+function THREERoot(params) {
+  params = utils.extend({
+    antialias:true,
+
+    fov:60,
+    zNear:1,
+    zFar:10000,
+
+    createCameraControls:true
+  }, params);
+
+  this.renderer = new THREE.WebGLRenderer({
+    antialias:params.antialias
   });
+  document.getElementById('three-container').appendChild(this.renderer.domElement);
 
-  return mesh;
-}
+  this.camera = new THREE.PerspectiveCamera(
+    params.fov,
+    window.innerWidth / window.innerHeight,
+    params.zNear,
+    params.zfar
+  );
 
-function generateTextGeometry(text, params) {
-  var geometry = new THREE.TextGeometry(text, params);
+  this.scene = new THREE.Scene();
 
-  geometry.computeBoundingBox();
-
-  geometry.userData = {};
-  geometry.userData.size = {
-    width: geometry.boundingBox.max.x - geometry.boundingBox.min.x,
-    height: geometry.boundingBox.max.y - geometry.boundingBox.min.y,
-    depth: geometry.boundingBox.max.z - geometry.boundingBox.min.z
-  };
-
-  var anchorX = geometry.userData.size.width * -params.anchor.x;
-  var anchorY = geometry.userData.size.height * -params.anchor.y;
-  var anchorZ = geometry.userData.size.depth * -params.anchor.z;
-  var matrix = new THREE.Matrix4().makeTranslation(anchorX, anchorY, anchorZ);
-
-  geometry.applyMatrix(matrix);
-
-  return geometry;
-}
-
-function generateBufferGeometry(geometry) {
-  var bufferGeometry = new THREE.BAS.ModelBufferGeometry(geometry);
-
-  var aAnimation = bufferGeometry.createAttribute('aAnimation', 2);
-  var aControl0 = bufferGeometry.createAttribute('aControl0', 3);
-  var aControl1 = bufferGeometry.createAttribute('aControl1', 3);
-  var aEndPosition = bufferGeometry.createAttribute('aEndPosition', 3);
-
-  var faceCount = bufferGeometry.faceCount;
-  var i, i2, i3, i4, v;
-
-  var size = geometry.userData.size;
-  var length = new THREE.Vector3(size.width, size.height, size.depth).multiplyScalar(0.5).length();
-  var maxDelay = length * 0.06;
-
-  bufferGeometry.animationDuration = maxDelay + 4 + 1;
-
-  for (i = 0, i2 = 0, i3 = 0, i4 = 0; i < faceCount; i++, i2 += 6, i3 += 9, i4 += 12) {
-    var face = geometry.faces[i];
-    var centroid = THREE.BAS.Utils.computeCentroid(geometry, face);
-    var dirX = centroid.x > 0 ? 1 : -1;
-    var dirY = centroid.y > 0 ? 1 : -1;
-
-    // animation
-    var delay = centroid.length() * THREE.Math.randFloat(0.03, 0.06);
-    var duration = THREE.Math.randFloat(2, 4);
-
-    for (v = 0; v < 6; v += 2) {
-      aAnimation.array[i2 + v    ] = delay + Math.random();
-      aAnimation.array[i2 + v + 1] = duration;
-    }
-
-    // ctrl
-    var c0x = THREE.Math.randFloat(0, 30) * dirX;
-    var c0y = THREE.Math.randFloat(60, 120) * dirY;
-    var c0z = THREE.Math.randFloat(-20, 20);
-
-    var c1x = THREE.Math.randFloat(30, 60) * dirX;
-    var c1y = THREE.Math.randFloat(0, 60) * dirY;
-    var c1z = THREE.Math.randFloat(-20, 20);
-
-    for (v = 0; v < 9; v += 3) {
-      aControl0.array[i3 + v    ] = c0x;
-      aControl0.array[i3 + v + 1] = c0y;
-      aControl0.array[i3 + v + 2] = c0z;
-
-      aControl1.array[i3 + v    ] = c1x;
-      aControl1.array[i3 + v + 1] = c1y;
-      aControl1.array[i3 + v + 2] = c1z;
-    }
+  if (params.createCameraControls) {
+    this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
   }
 
-  return bufferGeometry;
-}
+  this.resize = this.resize.bind(this);
+  this.tick = this.tick.bind(this);
 
-function tick() {
-  render();
-  requestAnimationFrame(tick);
-}
+  this.resize();
+  this.tick();
 
-function render() {
-  mRenderer.render(mScene, mCamera);
+  window.addEventListener('resize', this.resize, false);
 }
+THREERoot.prototype = {
+  tick: function() {
+    this.update();
+    this.render();
+    requestAnimationFrame(this.tick);
+  },
+  update: function() {
+    this.controls && this.controls.update();
+  },
+  render: function() {
+    this.renderer.render(this.scene, this.camera);
+  },
+  resize: function() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
 
-function resize() {
-  mCamera.aspect = window.innerWidth / window.innerHeight;
-  mCamera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+};
 
-  mRenderer.setSize(window.innerWidth, window.innerHeight);
-}
+////////////////////
+// UTILS
+////////////////////
+
+var utils = {
+  extend:function(dst, src) {
+    for (var key in src) {
+      dst[key] = src[key];
+    }
+
+    return dst;
+  }
+};
