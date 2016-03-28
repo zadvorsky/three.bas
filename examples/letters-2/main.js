20567a0ec5a6d461e42e23348c9c2a91c7e2d0dc
szenia <s.zadvornykh@gmail.com>
2016-03-28 11:03:49 -0400

    letters 2

--- examples/letters-2/main.js
+++ examples/letters-2/main.js
@@ -3,96 +3,44 @@ window.onload = init;
 function init() {
   var root = new THREERoot({
     createCameraControls:false,
-    fov:10
+    fov:60
   });
   root.renderer.setClearColor(0xffffff);
-  root.camera.position.set(0, 0, 1400);
+  root.camera.position.set(0, 0, 400);
 
   var textAnimation = createTextAnimation();
+  textAnimation.position.y = -40;
   root.scene.add(textAnimation);
 
-  var tween = TweenMax.fromTo(textAnimation, 4,
-    {animationProgress:0},
-    {animationProgress:1, ease:Power1.easeInOut, repeat:-1, yoyo:true}
+  var tl = new TimelineMax({
+    repeat:-1,
+    yoyo:true
+  });
+  tl.fromTo(textAnimation, 4,
+    {animationProgress:0.0},
+    {animationProgress:1.0, ease:Power1.easeInOut},
+    0
   );
-  createTweenScrubber(tween);
-}
+  // tl.fromTo(textAnimation.material.uniforms['opacity'], 3,
+  //   {value:1.0},
+  //   {value:0.0, ease:Power0.easeIn},
+  //   1
+  // );
 
-function createTweenScrubber(tween, seekSpeed) {
-  seekSpeed = seekSpeed || 0.001;
-
-  function stop() {
-    TweenMax.to(tween, 2, {timeScale:0});
-  }
-
-  function resume() {
-    TweenMax.to(tween, 2, {timeScale:1});
-  }
-
-  function seek(dx) {
-    var progress = tween.progress();
-    var p = THREE.Math.clamp((progress + (dx * seekSpeed)), 0, 1);
-
-    tween.progress(p);
-  }
-
-  var _cx = 0;
-
-  // desktop
-  var mouseDown = false;
-  document.body.style.cursor = 'pointer';
-
-  window.addEventListener('mousedown', function(e) {
-    mouseDown = true;
-    document.body.style.cursor = 'ew-resize';
-    _cx = e.clientX;
-    stop();
-  });
-  window.addEventListener('mouseup', function(e) {
-    mouseDown = false;
-    document.body.style.cursor = 'pointer';
-    resume();
-  });
-  window.addEventListener('mousemove', function(e) {
-    if (mouseDown === true) {
-      var cx = e.clientX;
-      var dx = cx - _cx;
-      _cx = cx;
-
-      seek(dx);
-    }
-  });
-  // mobile
-  window.addEventListener('touchstart', function(e) {
-    _cx = e.touches[0].clientX;
-    stop();
-    e.preventDefault();
-  });
-  window.addEventListener('touchend', function(e) {
-    resume();
-    e.preventDefault();
-  });
-  window.addEventListener('touchmove', function(e) {
-    var cx = e.touches[0].clientX;
-    var dx = cx - _cx;
-    _cx = cx;
-
-    seek(dx);
-    e.preventDefault();
-  });
+  createTweenScrubber(tl);
 }
 
 function createTextAnimation() {
-  var geometry = generateTextGeometry('PIECE BY PIECE', {
+  var geometry = generateTextGeometry('UP IN SMOKE', {
     size:14,
-    height:0,
+    height:2,
     font:'droid sans',
     weight:'bold',
     style:'normal',
     bevelSize:0.75,
     bevelThickness:0.50,
     bevelEnabled:true,
-    anchor:{x:0.5, y:0.5, z:0.5}
+    anchor:{x:0.5, y:0.0, z:0.5}
   });
 
   THREE.BAS.Utils.separateFaces(geometry);
@@ -130,6 +78,7 @@ function TextAnimation(textGeometry) {
   var bufferGeometry = new THREE.BAS.ModelBufferGeometry(textGeometry);
 
   var aAnimation = bufferGeometry.createAttribute('aAnimation', 2);
+  var aCentroid = bufferGeometry.createAttribute('aCentroid', 3);
   var aControl0 = bufferGeometry.createAttribute('aControl0', 3);
   var aControl1 = bufferGeometry.createAttribute('aControl1', 3);
   var aEndPosition = bufferGeometry.createAttribute('aEndPosition', 3);
@@ -138,35 +87,46 @@ function TextAnimation(textGeometry) {
   var i, i2, i3, i4, v;
 
   var size = textGeometry.userData.size;
-  var length = new THREE.Vector3(size.width, size.height, size.depth).multiplyScalar(0.5).length();
-  var maxDelay = length * 0.06;
 
-  this.animationDuration = maxDelay + 4 + 1;
+  var maxDelayX = 2.0;
+  var maxDelayY = 0.25;
+  var minDuration = 4;
+  var maxDuration = 8;
+  var stretch = 0.15;
+
+  this.animationDuration = maxDelayX + maxDelayY + maxDuration + stretch;
+  this.animationDuration = 4;
   this._animationProgress = 0;
 
   for (i = 0, i2 = 0, i3 = 0, i4 = 0; i < faceCount; i++, i2 += 6, i3 += 9, i4 += 12) {
     var face = textGeometry.faces[i];
     var centroid = THREE.BAS.Utils.computeCentroid(textGeometry, face);
-    var dirX = centroid.x > 0 ? 1 : -1;
-    var dirY = centroid.y > 0 ? 1 : -1;
 
     // animation
-    var delay = centroid.length() * THREE.Math.randFloat(0.03, 0.06);
-    var duration = THREE.Math.randFloat(2, 4);
+    var delayX = Math.max(0, (centroid.x / size.width) * maxDelayX);
+    var delayY = Math.max(0, (1.0 - (centroid.y / size.height)) * maxDelayY);
+    var duration = THREE.Math.randFloat(minDuration, maxDuration);
 
     for (v = 0; v < 6; v += 2) {
-      aAnimation.array[i2 + v    ] = delay + Math.random();
+      aAnimation.array[i2 + v    ] = delayX + delayY + Math.random() * stretch;
       aAnimation.array[i2 + v + 1] = duration;
     }
 
-    // ctrl
-    var c0x = THREE.Math.randFloat(0, 30) * dirX;
-    var c0y = THREE.Math.randFloat(60, 120) * dirY;
-    var c0z = THREE.Math.randFloat(-20, 20);
+    // centroid
+    for (v = 0; v < 9; v += 3) {
+      aCentroid.array[i3 + v    ] = centroid.x;
+      aCentroid.array[i3 + v + 1] = centroid.y;
+      aCentroid.array[i3 + v + 2] = centroid.z;
+    }
 
-    var c1x = THREE.Math.randFloat(30, 60) * dirX;
-    var c1y = THREE.Math.randFloat(0, 60) * dirY;
-    var c1z = THREE.Math.randFloat(-20, 20);
+    // ctrl
+    var c0x = centroid.x + THREE.Math.randFloat(40, 120);
+    var c0y = centroid.y + size.height * THREE.Math.randFloat(0.0, 12.0);
+    var c0z = THREE.Math.randFloatSpread(120);
+
+    var c1x = centroid.x + THREE.Math.randFloat(80, 120) * -1;
+    var c1y = centroid.y + size.height * THREE.Math.randFloat(0.0, 12.0);
+    var c1z = THREE.Math.randFloatSpread(120);
 
     for (v = 0; v < 9; v += 3) {
       aControl0.array[i3 + v    ] = c0x;
@@ -177,20 +137,36 @@ function TextAnimation(textGeometry) {
       aControl1.array[i3 + v + 1] = c1y;
       aControl1.array[i3 + v + 2] = c1z;
     }
+
+    // end position
+    var x, y, z;
+
+    x = centroid.x + THREE.Math.randFloatSpread(120);
+    y = centroid.y + size.height * THREE.Math.randFloat(4.0, 12.0);
+    z = THREE.Math.randFloat(-20, 20);
+
+    for (v = 0; v < 9; v += 3) {
+      aEndPosition.array[i3 + v    ] = x;
+      aEndPosition.array[i3 + v + 1] = y;
+      aEndPosition.array[i3 + v + 2] = z;
+    }
   }
 
   var material = new THREE.BAS.BasicAnimationMaterial({
       shading: THREE.FlatShading,
       side: THREE.DoubleSide,
+      transparent: true,
       uniforms: {
         uTime: {type: 'f', value: 0}
       },
       shaderFunctions: [
-        THREE.BAS.ShaderChunk['cubic_bezier']
+        THREE.BAS.ShaderChunk['cubic_bezier'],
+        THREE.BAS.ShaderChunk['ease_out_cubic']
       ],
       shaderParameters: [
         'uniform float uTime;',
         'attribute vec2 aAnimation;',
+        'attribute vec3 aCentroid;',
         'attribute vec3 aControl0;',
         'attribute vec3 aControl1;',
         'attribute vec3 aEndPosition;'
@@ -199,13 +175,21 @@ function TextAnimation(textGeometry) {
         'float tDelay = aAnimation.x;',
         'float tDuration = aAnimation.y;',
         'float tTime = clamp(uTime - tDelay, 0.0, tDuration);',
-        'float tProgress = tTime / tDuration;'
+        'float tProgress =  ease(tTime, 0.0, 1.0, tDuration);'
+        // 'float tProgress = tTime / tDuration;'
       ],
       shaderTransformPosition: [
-        'vec3 tPosition = transformed;',
+        'vec3 tPosition = transformed - aCentroid;',
         'tPosition *= 1.0 - tProgress;',
-        'tPosition += cubicBezier(transformed, aControl0, aControl1, aEndPosition, tProgress);',
+        'tPosition += aCentroid;',
+        'tPosition += cubicBezier(tPosition, aControl0, aControl1, aEndPosition, tProgress);',
         'transformed = tPosition;'
+
+        // 'vec3 tPosition = transformed;',
+        // 'tPosition *= 1.0 - tProgress;',
+        // 'tPosition += cubicBezier(transformed, aControl0, aControl1, aEndPosition, tProgress);',
+        // 'tPosition += mix(transformed, aEndPosition, tProgress);',
+        // 'transformed = tPosition;'
       ]
     },
     {
@@ -298,5 +282,72 @@ var utils = {
     }
 
     return dst;
+  },
+  randSign: function() {
+    return Math.random() > 0.5 ? 1 : -1;
   }
 };
+
+function createTweenScrubber(tween, seekSpeed) {
+  seekSpeed = seekSpeed || 0.001;
+
+  function stop() {
+    TweenMax.to(tween, 2, {timeScale:0});
+  }
+
+  function resume() {
+    TweenMax.to(tween, 2, {timeScale:1});
+  }
+
+  function seek(dx) {
+    var progress = tween.progress();
+    var p = THREE.Math.clamp((progress + (dx * seekSpeed)), 0, 1);
+
+    tween.progress(p);
+  }
+
+  var _cx = 0;
+
+  // desktop
+  var mouseDown = false;
+  document.body.style.cursor = 'pointer';
+
+  window.addEventListener('mousedown', function(e) {
+    mouseDown = true;
+    document.body.style.cursor = 'ew-resize';
+    _cx = e.clientX;
+    stop();
+  });
+  window.addEventListener('mouseup', function(e) {
+    mouseDown = false;
+    document.body.style.cursor = 'pointer';
+    resume();
+  });
+  window.addEventListener('mousemove', function(e) {
+    if (mouseDown === true) {
+      var cx = e.clientX;
+      var dx = cx - _cx;
+      _cx = cx;
+
+      seek(dx);
+    }
+  });
+  // mobile
+  window.addEventListener('touchstart', function(e) {
+    _cx = e.touches[0].clientX;
+    stop();
+    e.preventDefault();
+  });
+  window.addEventListener('touchend', function(e) {
+    resume();
+    e.preventDefault();
+  });
+  window.addEventListener('touchmove', function(e) {
+    var cx = e.touches[0].clientX;
+    var dx = cx - _cx;
+    _cx = cx;
+
+    seek(dx);
+    e.preventDefault();
+  });
+}
