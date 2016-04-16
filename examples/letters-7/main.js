ed086d8a81086b52444b556a91dcc327281d9a09
Szenia Zadvornykh <s.zadvornykh@gmail.com>
2016-04-16 14:52:41 -0400

    weird version

--- examples/letters-7/main.js
+++ examples/letters-7/main.js
@@ -6,7 +6,7 @@ var settings = {
 
 function init() {
   var root = new THREERoot({
-    createCameraControls:!true,
+    createCameraControls:true,
     antialias:(window.devicePixelRatio === 1),
     fov:80
   });
@@ -25,9 +25,6 @@ function init() {
     var textAnimation = new TextAnimation(textAnimationData);
     group.add(textAnimation);
 
-    var explosionAnimation = new ExplosionAnimation(textAnimationData);
-    group.add(explosionAnimation);
-
     var box = textAnimationData.geometry.boundingBox;
     group.position.copy(box.size()).multiplyScalar(-0.5);
 
@@ -35,26 +32,8 @@ function init() {
     light.position.set(0, 0, 1);
     root.scene.add(light);
 
-    var lightTl = new TimelineMax();
-
-    var maxTime = Math.max(textAnimation.animationDuration, explosionAnimation.animationDuration);
+    var maxTime = textAnimation.animationDuration;
     var duration = 6.0;
-
-    for (var i = 0; i < textAnimationData.info.length; i++) {
-      var color = textAnimationData.info[i].color;
-      var pos = textAnimationData.info[i].boundingBox.center();
-
-      pos.x += textAnimationData.info[i].glyphOffset;
-
-      light = new THREE.PointLight(color, 2.0, 80, 2);
-      light.position.copy(pos);
-
-      group.add(light);
-
-      lightTl.fromTo(light, 0.4,
-        {intensity:0}, {intensity:4.0, repeat:1, yoyo:true},
-        (i * settings.letterTimeOffset) * (duration / maxTime));
-    }
 
     var tl = new TimelineMax({
       repeat:-1,
@@ -62,15 +41,13 @@ function init() {
       yoyo:true
     });
     tl.to(textAnimation, duration, {time:maxTime, ease:Power0.easeIn}, 0);
-    tl.to(explosionAnimation, duration, {time:maxTime, ease:Power0.easeIn}, 0);
-    tl.add(lightTl, 0);
 
-    createTweenScrubber(tl);
+    //createTweenScrubber(tl);
   });
 }
 
 function createTextAnimation(font) {
-  var text = 'BLOWN APART';
+  var text = 'TEST';
   var params = {
     size:36,
     height:8,
@@ -116,7 +93,7 @@ function generateSplitTextGeometry(text, params) {
     offset += glyph.ha * scale;
 
     // store face index offsets
-    THREE.BAS.Utils.tessellate(charGeometry, 1.0);
+    //THREE.BAS.Utils.tessellate(charGeometry, 1.0);
     THREE.BAS.Utils.separateFaces(charGeometry);
 
     data.info[i].faceCount = charGeometry.faces.length;
@@ -149,10 +126,11 @@ function TextAnimation(data) {
   var aAnimation = bufferGeometry.createAttribute('aAnimation', 2);
   var aStartPosition = bufferGeometry.createAttribute('aStartPosition', 3);
   var aEndPosition = bufferGeometry.createAttribute('aEndPosition', 3);
+  var aPivot = bufferGeometry.createAttribute('aPivot', 3);
   var aAxisAngle = bufferGeometry.createAttribute('aAxisAngle', 4);
 
   var minDuration = 1.0;
-  var maxDuration = 2.0;
+  var maxDuration = 1.0;
 
   this.animationDuration = maxDuration + data.info.length * settings.letterTimeOffset;
 
@@ -161,8 +139,9 @@ function TextAnimation(data) {
 
   var glyphSize = new THREE.Vector3();
   var glyphCenter = new THREE.Vector3();
-  var centroidLocal = new THREE.Vector3();
   var delta = new THREE.Vector3();
+
+  var keys = ['a', 'b', 'c'];
 
   for (var f = 0; f < data.info.length; f++) {
     bufferChar(data.info[f], f);
@@ -176,10 +155,22 @@ function TextAnimation(data) {
 
     box.size(glyphSize);
     box.center(glyphCenter);
+    glyphCenter.x += glyphOffset;
 
     var i, i2, i3, i4, v;
 
     var delay = index * settings.letterTimeOffset;
+
+    var pivot = new THREE.Vector3(
+      THREE.Math.randFloatSpread(100),
+      THREE.Math.randFloatSpread(100),
+      THREE.Math.randFloatSpread(100)
+    );
+    axis.x = THREE.Math.randFloatSpread(2);
+    axis.y = THREE.Math.randFloatSpread(2);
+    axis.z = THREE.Math.randFloatSpread(2);
+    axis.normalize();
+    angle = Math.PI * THREE.Math.randFloat(4.0, 8.0);
 
     for (i = s, i2 = s * 6, i3 = s * 9, i4 = s * 12; i < l; i++, i2 += 6, i3 += 9, i4 += 12) {
 
@@ -191,7 +182,16 @@ function TextAnimation(data) {
       var duration = THREE.Math.randFloat(minDuration, maxDuration);
 
       for (v = 0; v < 6; v += 2) {
-        aAnimation.array[i2 + v    ] = delay;
+        var vertex = textGeometry.vertices[face[keys[v / 2]]];
+
+        delta.subVectors(vertex, glyphCenter);
+
+        //var vertexDelay = (glyphSize.y * 0.5 - Math.abs(delta.y)) * 0.02;
+        var vertexDelay = (glyphSize.length() - delta.length()) * 0.02;
+
+        if (vertexDelay < 0) console.log('wat');
+
+        aAnimation.array[i2 + v    ] = delay + vertexDelay;
         aAnimation.array[i2 + v + 1] = duration;
       }
 
@@ -201,29 +201,22 @@ function TextAnimation(data) {
         aStartPosition.array[i3 + v + 1] = centroid.y;
         aStartPosition.array[i3 + v + 2] = centroid.z;
       }
-
       // end position
-      centroidLocal.copy(centroid);
-      centroidLocal.x -= glyphOffset;
-      delta.subVectors(centroidLocal, glyphCenter);
-
-      var x = delta.x * THREE.Math.randFloat(4.0, 12.0);
-      var y = delta.y * THREE.Math.randFloat(4.0, 12.0);
-      var z = delta.z * THREE.Math.randFloat(4.0, 12.0);
+      var dy = (centroid.y > glyphCenter.y) ? 1 : -1;
 
       for (v = 0; v < 9; v += 3) {
-        aEndPosition.array[i3 + v    ] = centroid.x + x;
-        aEndPosition.array[i3 + v + 1] = centroid.y + y;
-        aEndPosition.array[i3 + v + 2] = centroid.z + z;
+        aEndPosition.array[i3 + v    ] = centroid.x;
+        aEndPosition.array[i3 + v + 1] = centroid.y + dy * 100;
+        aEndPosition.array[i3 + v + 2] = centroid.z;
+      }
+
+      for (v = 0; v < 9; v += 3) {
+        aPivot.array[i3 + v    ] = pivot.x;
+        aPivot.array[i3 + v + 1] = pivot.y;
+        aPivot.array[i3 + v + 2] = pivot.z;
       }
 
       // axis angle
-      axis.x = THREE.Math.randFloatSpread(2);
-      axis.y = THREE.Math.randFloatSpread(2);
-      axis.z = THREE.Math.randFloatSpread(2);
-      axis.normalize();
-      angle = Math.PI * THREE.Math.randFloat(4.0, 8.0);
-
       for (v = 0; v < 12; v += 4) {
         aAxisAngle.array[i4 + v    ] = axis.x;
         aAxisAngle.array[i4 + v + 1] = axis.y;
@@ -252,6 +245,7 @@ function TextAnimation(data) {
         'attribute vec2 aAnimation;',
         'attribute vec3 aStartPosition;',
         'attribute vec3 aEndPosition;',
+        'attribute vec3 aPivot;',
         'attribute vec4 aAxisAngle;'
       ],
       shaderVertexInit: [
@@ -263,7 +257,7 @@ function TextAnimation(data) {
       ],
       shaderTransformPosition: [
         // scale
-        'transformed *= 1.0 - tProgress;',
+        //'transformed *= 1.0 - tProgress;',
 
         // rotate
         'float angle = aAxisAngle.w * tProgress;',
@@ -278,7 +272,7 @@ function TextAnimation(data) {
       //diffuse: 0x444444,
       //specular: 0xcccccc,
       //shininess: 4,
-      //emissive: 0x444444
+      emissive: 0x444444
     }
   );
 
@@ -324,204 +318,6 @@ TextAnimationGeometry.prototype.bufferPositions = function() {
     positionBuffer[face.c * 3    ] = c.x - centroid.x;
     positionBuffer[face.c * 3 + 1] = c.y - centroid.y;
     positionBuffer[face.c * 3 + 2] = c.z - centroid.z;
-  }
-};
-
-
-function ExplosionAnimation(data) {
-  var letterCount = data.info.length;
-  var prefabsPerLetter = 750;
-  var prefabCount = prefabsPerLetter * letterCount;
-  var prefabSize = 2.0;
-
-  // var prefabGeometry = new THREE.TetrahedronGeometry(prefabSize);
-  var prefabGeometry = new THREE.PlaneGeometry(prefabSize, prefabSize, 1, 4);
-
-  var geometry = new ExplosionAnimationGeometry(prefabGeometry, prefabCount);
-
-  var aDelayDuration = geometry.createAttribute('aDelayDuration', 2);
-  var aColor = geometry.createAttribute('color', 3);
-  var aStartPosition = geometry.createAttribute('aStartPosition', 3);
-  var aControlPosition0 = geometry.createAttribute('aControlPosition0', 3);
-  var aControlPosition1 = geometry.createAttribute('aControlPosition1', 3);
-  var aEndPosition = geometry.createAttribute('aEndPosition', 3);
-  var aAxisAngle = geometry.createAttribute('aAxisAngle', 4);
-
-  var duration, delay;
-  var minDuration = 0.25;
-  var maxDuration = 0.5;
-  var vertexDelay = 0.0125;
-
-  this.animationDuration = (maxDuration + vertexDelay * prefabGeometry.vertices.length) * letterCount * settings.letterTimeOffset;
-
-  var glyphSize = new THREE.Vector3();
-  var glyphCenter = new THREE.Vector3();
-
-  for (var index = 0; index < letterCount; index++) {
-    var letterOffset = prefabsPerLetter * index;
-    var letterBox = data.info[index].boundingBox;
-    letterBox.size(glyphSize);
-    letterBox.center(glyphCenter);
-    glyphCenter.x += data.info[index].glyphOffset;
-
-    var i, j, offset;
-
-    delay = index * settings.letterTimeOffset;
-
-    for (i = 0, offset = letterOffset * prefabGeometry.vertices.length * 2; i < prefabCount; i++) {
-      duration = THREE.Math.randFloat(minDuration, maxDuration);
-
-      for (j = 0; j < prefabGeometry.vertices.length; j++) {
-
-        aDelayDuration.array[offset++] = delay + vertexDelay * j * duration;
-        aDelayDuration.array[offset++] = duration;
-      }
-    }
-
-    var color = data.info[index].color;
-    var colorHSL = color.getHSL();
-    var h, s, l;
-
-    for (i = 0, offset = letterOffset * prefabGeometry.vertices.length * 3; i < prefabCount; i++) {
-      h = colorHSL.h;
-      s = THREE.Math.randFloat(0.50, 1.00);
-      l = THREE.Math.randFloat(0.25, 0.75);
-      color.setHSL(h, s, l);
-
-      for (j = 0; j < geometry.prefabVertexCount; j++) {
-        aColor.array[offset  ] = color.r;
-        aColor.array[offset+1] = color.g;
-        aColor.array[offset+2] = color.b;
-
-        offset += 3;
-      }
-    }
-
-    var u, v, sp, ep, cp0, cp1;
-
-    for (i = 0, offset = letterOffset * prefabGeometry.vertices.length * 3; i < prefabCount; i++) {
-      sp = glyphCenter;
-
-      u = Math.random();
-      v = Math.random();
-      ep = utils.spherePoint(u, v);
-      ep.x *= THREE.Math.randFloat(40, 80);
-      ep.y *= THREE.Math.randFloat(80, 120);
-      ep.z *= THREE.Math.randFloat(40, 80);
-
-      u *= THREE.Math.randFloat(0.8, 1.2);
-      v *= THREE.Math.randFloat(0.8, 1.2);
-      cp0 = utils.spherePoint(u, v);
-      cp0.x *= THREE.Math.randFloat(10, 20);
-      cp0.y *= THREE.Math.randFloat(40, 60);
-      cp0.z *= THREE.Math.randFloat(10, 20);
-
-      u *= THREE.Math.randFloat(0.8, 1.2);
-      v *= THREE.Math.randFloat(0.8, 1.2);
-      cp1 = utils.spherePoint(u, v);
-      cp1.x *= THREE.Math.randFloat(20, 40);
-      cp1.y *= THREE.Math.randFloat(60, 80);
-      cp1.z *= THREE.Math.randFloat(20, 40);
-
-      for (j = 0; j < prefabGeometry.vertices.length; j++) {
-        aStartPosition.array[offset  ] = sp.x;
-        aStartPosition.array[offset+1] = sp.y;
-        aStartPosition.array[offset+2] = sp.z;
-
-        aEndPosition.array[offset  ] = sp.x + ep.x;
-        aEndPosition.array[offset+1] = sp.y + ep.y;
-        aEndPosition.array[offset+2] = sp.z + ep.z;
-
-        aControlPosition0.array[offset  ] = sp.x + cp0.x;
-        aControlPosition0.array[offset+1] = sp.y + cp0.y;
-        aControlPosition0.array[offset+2] = sp.z + cp0.z;
-
-        aControlPosition1.array[offset  ] = sp.x + cp1.x;
-        aControlPosition1.array[offset+1] = sp.y + cp1.y;
-        aControlPosition1.array[offset+2] = sp.z + cp1.z;
-
-        offset += 3;
-      }
-    }
-  }
-
-  var material = new THREE.BAS.BasicAnimationMaterial({
-      vertexColors: THREE.VertexColors,
-      side:THREE.DoubleSide,
-      uniforms: {
-        uTime: {type: 'f', value: 0},
-        uScale: {type: 'f', value: 1.0}
-      },
-      shaderFunctions: [
-        THREE.BAS.ShaderChunk['quaternion_rotation'],
-        THREE.BAS.ShaderChunk['cubic_bezier'],
-        THREE.BAS.ShaderChunk['ease_out_cubic']
-      ],
-      shaderParameters: [
-        'uniform float uTime;',
-
-        'attribute vec2 aDelayDuration;',
-        'attribute vec3 aStartPosition;',
-        'attribute vec3 aControlPosition0;',
-        'attribute vec3 aControlPosition1;',
-        'attribute vec3 aEndPosition;',
-        'attribute vec4 aAxisAngle;'
-      ],
-      shaderVertexInit: [
-        'float tDelay = aDelayDuration.x;',
-        'float tDuration = aDelayDuration.y;',
-        'float tTime = clamp(uTime - tDelay, 0.0, tDuration);',
-        //'float tProgress = ease(tTime, 0.0, 1.0, tDuration);',
-        'float tProgress = tTime / tDuration;'
-      ],
-      shaderTransformPosition: [
-        'float scl = tProgress * 2.0 - 1.0;',
-        'transformed *= (1.0 - scl * scl);',
-        'transformed += cubicBezier(aStartPosition, aControlPosition0, aControlPosition1, aEndPosition, tProgress);'
-      ]
-    },{}
-  );
-
-  THREE.Mesh.call(this, geometry, material);
-  this.frustumCulled = false;
-}
-ExplosionAnimation.prototype = Object.create(THREE.Mesh.prototype);
-ExplosionAnimation.prototype.constructor = ExplosionAnimation;
-Object.defineProperty(ExplosionAnimation.prototype, 'time', {
-  get: function() {
-    return this.material.uniforms['uTime'].value;
-  },
-  set: function(v) {
-    this.material.uniforms['uTime'].value = v;
-  }
-});
-
-function ExplosionAnimationGeometry(prefab, count) {
-  THREE.BAS.PrefabBufferGeometry.call(this, prefab, count);
-}
-ExplosionAnimationGeometry.prototype = Object.create(THREE.BAS.PrefabBufferGeometry.prototype);
-ExplosionAnimationGeometry.prototype.constructor = ExplosionAnimationGeometry;
-ExplosionAnimationGeometry.prototype.bufferPositions = function() {
-  var positionBuffer = this.createAttribute('position', 3).array;
-
-  var scaleMatrix = new THREE.Matrix4();
-  var scale;
-  var p = new THREE.Vector3();
-
-  for (var i = 0, offset = 0; i < this.prefabCount; i++) {
-    for (var j = 0; j < this.prefabVertexCount; j++, offset += 3) {
-      var prefabVertex = this.prefabGeometry.vertices[j];
-
-      scale = Math.random();
-      scaleMatrix.identity().makeScale(scale, scale, scale);
-
-      p.copy(prefabVertex);
-      p.applyMatrix4(scaleMatrix);
-
-      positionBuffer[offset    ] = p.x;
-      positionBuffer[offset + 1] = p.y;
-      positionBuffer[offset + 2] = p.z;
-    }
   }
 };
 
