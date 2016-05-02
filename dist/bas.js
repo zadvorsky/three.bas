f0ea7393019bfaf5f9fb8aafcdba356201dfeb2b
Szenia Zadvornykh <s.zadvornykh@gmail.com>
2016-05-02 18:24:26 -0400

    gallery example start + uv things

--- dist/bas.js
+++ dist/bas.js
@@ -632,11 +632,33 @@ THREE.BAS.ModelBufferGeometry.prototype.bufferPositions = function() {
   var positionBuffer = this.createAttribute('position', 3).array;
 
   for (var i = 0, offset = 0; i < this.vertexCount; i++, offset += 3) {
-    var prefabVertex = this.modelGeometry.vertices[i];
+    var vertex = this.modelGeometry.vertices[i];
 
-    positionBuffer[offset    ] = prefabVertex.x;
-    positionBuffer[offset + 1] = prefabVertex.y;
-    positionBuffer[offset + 2] = prefabVertex.z;
+    positionBuffer[offset    ] = vertex.x;
+    positionBuffer[offset + 1] = vertex.y;
+    positionBuffer[offset + 2] = vertex.z;
+  }
+};
+
+THREE.BAS.ModelBufferGeometry.prototype.bufferUVs = function() {
+  var uvBuffer = this.createAttribute('uv', 2).array;
+
+  for (var i = 0; i < this.faceCount; i++) {
+
+    var face = this.modelGeometry.faces[i];
+    var uv;
+
+    uv = this.modelGeometry.faceVertexUvs[0][i][0];
+    uvBuffer[face.a * 2]     = uv.x;
+    uvBuffer[face.a * 2 + 1] = uv.y;
+
+    uv = this.modelGeometry.faceVertexUvs[0][i][1];
+    uvBuffer[face.b * 2]     = uv.x;
+    uvBuffer[face.b * 2 + 1] = uv.y;
+
+    uv = this.modelGeometry.faceVertexUvs[0][i][2];
+    uvBuffer[face.c * 2]     = uv.x;
+    uvBuffer[face.c * 2 + 1] = uv.y;
   }
 };
 
---
+++ examples/gallery/main.js
@@ -0,0 +1,272 @@
+window.onload = init;
+
+function init() {
+  var root = new THREERoot({
+    createCameraControls: true,
+    antialias: (window.devicePixelRatio === 1),
+    fov: 80
+  });
+
+  root.renderer.setClearColor(0x000000);
+  root.renderer.setPixelRatio(window.devicePixelRatio || 1);
+  root.camera.position.set(0, 0, 250);
+
+  var light = new THREE.DirectionalLight();
+  light.position.set(0, 0, 1);
+  root.scene.add(light);
+
+  var width = 100;
+  var height = 60;
+  var slide = new Slide(width, height);
+  root.scene.add(slide);
+}
+
+////////////////////
+// CLASSES
+////////////////////
+
+function Slide(width, height) {
+  var plane = new THREE.PlaneGeometry(width, height, width, height);
+
+  THREE.BAS.Utils.separateFaces(plane);
+
+  var geometry = new SlideGeometry(plane);
+
+  geometry.bufferUVs();
+
+  console.log(geometry);
+
+  var aAnimation = geometry.createAttribute('aAnimation', 2);
+  var aStartPosition = geometry.createAttribute('aStartPosition', 3);
+  var aEndPosition = geometry.createAttribute('aEndPosition', 3);
+
+  var i, i2, i3, i4, v;
+
+  var minDuration = 1.0;
+  var maxDuration = 2.0;
+
+  for (i = 0, i2 = 0, i3 = 0, i4 = 0; i < geometry.faceCount; i++, i2 += 6, i3 += 9, i4 += 12) {
+    var face = plane.faces[i];
+    var centroid = THREE.BAS.Utils.computeCentroid(plane, face);
+
+    // animation
+    var duration = THREE.Math.randFloat(minDuration, maxDuration);
+
+    for (v = 0; v < 6; v += 2) {
+      aAnimation.array[i2 + v]     = 0;
+      aAnimation.array[i2 + v + 1] = duration;
+    }
+
+    // start position (centroid)
+
+    for (v = 0; v < 9; v += 3) {
+      aStartPosition.array[i3 + v]     = centroid.x;
+      aStartPosition.array[i3 + v + 1] = centroid.y;
+      aStartPosition.array[i3 + v + 2] = centroid.z;
+    }
+
+    // end position
+
+    for (v = 0; v < 9; v += 3) {
+      aEndPosition.array[i3 + v]     = centroid.x;
+      aEndPosition.array[i3 + v + 1] = centroid.y;
+      aEndPosition.array[i3 + v + 2] = centroid.z;
+    }
+  }
+
+  var material = new THREE.BAS.PhongAnimationMaterial(
+    {
+      shading: THREE.FlatShading,
+      side: THREE.DoubleSide,
+      uniforms: {
+        uTime: {type: 'f', value: 0}
+      },
+      shaderFunctions: [
+        THREE.BAS.ShaderChunk['cubic_bezier'],
+        THREE.BAS.ShaderChunk['ease_out_cubic'],
+        THREE.BAS.ShaderChunk['quaternion_rotation']
+      ],
+      shaderParameters: [
+        'uniform float uTime;',
+        'attribute vec2 aAnimation;',
+        'attribute vec3 aStartPosition;',
+        'attribute vec3 aEndPosition;',
+      ],
+      shaderVertexInit: [
+        'float tDelay = aAnimation.x;',
+        'float tDuration = aAnimation.y;',
+        'float tTime = clamp(uTime - tDelay, 0.0, tDuration);',
+        //'float tProgress = ease(tTime, 0.0, 1.0, tDuration);'
+        'float tProgress = tTime / tDuration;'
+      ],
+      shaderTransformPosition: [
+        'transformed += mix(aStartPosition, aEndPosition, tProgress);',
+      ]
+    },
+    {
+      map: new THREE.TextureLoader().load('image.jpg')
+    }
+  );
+
+  THREE.Mesh.call(this, geometry, material);
+
+  this.frustumCulled = false;
+}
+Slide.prototype = Object.create(THREE.Mesh.prototype);
+Slide.prototype.constructor = Slide;
+Object.defineProperty(Slide.prototype, 'time', {
+  get: function () {
+    return this.material.uniforms['uTime'].value;
+  },
+  set: function (v) {
+    this.material.uniforms['uTime'].value = v;
+  }
+});
+
+function SlideGeometry(model) {
+  THREE.BAS.ModelBufferGeometry.call(this, model);
+}
+SlideGeometry.prototype = Object.create(THREE.BAS.ModelBufferGeometry.prototype);
+SlideGeometry.prototype.constructor = SlideGeometry;
+SlideGeometry.prototype.bufferPositions = function () {
+  var positionBuffer = this.createAttribute('position', 3).array;
+
+  for (var i = 0; i < this.faceCount; i++) {
+    var face = this.modelGeometry.faces[i];
+    var centroid = THREE.BAS.Utils.computeCentroid(this.modelGeometry, face);
+
+    var a = this.modelGeometry.vertices[face.a];
+    var b = this.modelGeometry.vertices[face.b];
+    var c = this.modelGeometry.vertices[face.c];
+
+    positionBuffer[face.a * 3]     = a.x - centroid.x;
+    positionBuffer[face.a * 3 + 1] = a.y - centroid.y;
+    positionBuffer[face.a * 3 + 2] = a.z - centroid.z;
+
+    positionBuffer[face.b * 3]     = b.x - centroid.x;
+    positionBuffer[face.b * 3 + 1] = b.y - centroid.y;
+    positionBuffer[face.b * 3 + 2] = b.z - centroid.z;
+
+    positionBuffer[face.c * 3]     = c.x - centroid.x;
+    positionBuffer[face.c * 3 + 1] = c.y - centroid.y;
+    positionBuffer[face.c * 3 + 2] = c.z - centroid.z;
+  }
+};
+
+
+function THREERoot(params) {
+  params = utils.extend({
+    fov: 60,
+    zNear: 10,
+    zFar: 100000,
+
+    createCameraControls: true
+  }, params);
+
+  this.renderer = new THREE.WebGLRenderer({
+    antialias: params.antialias
+  });
+  this.renderer.setPixelRatio(Math.min(2, window.devicePixelRatio || 1));
+  document.getElementById('three-container').appendChild(this.renderer.domElement);
+
+  this.camera = new THREE.PerspectiveCamera(
+    params.fov,
+    window.innerWidth / window.innerHeight,
+    params.zNear,
+    params.zfar
+  );
+
+  this.scene = new THREE.Scene();
+
+  if (params.createCameraControls) {
+    this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+  }
+
+  this.resize = this.resize.bind(this);
+  this.tick = this.tick.bind(this);
+
+  this.resize();
+  this.tick();
+
+  window.addEventListener('resize', this.resize, false);
+}
+THREERoot.prototype = {
+  tick: function () {
+    this.update();
+    this.render();
+    requestAnimationFrame(this.tick);
+  },
+  update: function () {
+    this.controls && this.controls.update();
+  },
+  render: function () {
+    this.renderer.render(this.scene, this.camera);
+  },
+  resize: function () {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+};
+
+////////////////////
+// UTILS
+////////////////////
+
+var utils = {
+  extend: function (dst, src) {
+    for (var key in src) {
+      dst[key] = src[key];
+    }
+
+    return dst;
+  },
+  randSign: function () {
+    return Math.random() > 0.5 ? 1 : -1;
+  },
+  ease: function (ease, t, b, c, d) {
+    return b + ease.getRatio(t / d) * c;
+  },
+  fibSpherePoint: (function () {
+    var vec = {x: 0, y: 0, z: 0};
+    var G = Math.PI * (3 - Math.sqrt(5));
+
+    return function (i, n, radius) {
+      var step = 2.0 / n;
+      var r, phi;
+
+      vec.y = i * step - 1 + (step * 0.5);
+      r = Math.sqrt(1 - vec.y * vec.y);
+      phi = i * G;
+      vec.x = Math.cos(phi) * r;
+      vec.z = Math.sin(phi) * r;
+
+      radius = radius || 1;
+
+      vec.x *= radius;
+      vec.y *= radius;
+      vec.z *= radius;
+
+      return vec;
+    }
+  })(),
+  spherePoint: (function () {
+    return function (u, v) {
+      u === undefined && (u = Math.random());
+      v === undefined && (v = Math.random());
+
+      var theta = 2 * Math.PI * u;
+      var phi = Math.acos(2 * v - 1);
+
+      var vec = {};
+      vec.x = (Math.sin(phi) * Math.cos(theta));
+      vec.y = (Math.sin(phi) * Math.sin(theta));
+      vec.z = (Math.cos(phi));
+
+      return vec;
+    }
+  })()
+};
+
+
