36e51a1cf0a0e90ae672195840b76a7b21103c95
szenia <s.zadvornykh@gmail.com>
2016-07-01 15:08:30 -0400

    rough proof of concept

--- examples/motion_smear/main.js
+++ examples/motion_smear/main.js
@@ -3,8 +3,24 @@ window.onload = init;
 function init() {
   var root = new THREERoot();
   root.renderer.setClearColor(0xffffff);
-  root.camera.position.set(0, 0, 80);
+  root.camera.position.set(0, 0, 120);
   root.controls.autoRotate = false;
+
+  root.renderer.domElement.addEventListener( 'mousemove', onDocumentMouseMove, false );
+  root.renderer.domElement.addEventListener( 'mousedown', onDocumentMouseDown, false );
+  root.renderer.domElement.addEventListener( 'mouseup', onDocumentMouseUp, false );
+
+  window.controls = root.controls;
+  window.camera = root.camera;
+  window.container = root.container;
+
+  window.mouse = new THREE.Vector2();
+  window.raycaster = new THREE.Raycaster();
+  window.plane = new THREE.Plane();
+  window.offset = new THREE.Vector3();
+  window.intersection = new THREE.Vector3();
+  window.INTERSECTED = false;
+  window.SELECTED = false;
 
   var light = new THREE.DirectionalLight();
   light.position.set(0, 0, 1);
@@ -17,48 +33,151 @@ function init() {
   var animation = new Animation();
   root.add(animation);
 
-  animation.moveTo(new THREE.Vector3(50, 0, 0));
+  window.objects = [animation];
 
-
-  root.add(new THREE.AxisHelper(50))
+  // var p = new THREE.Vector3();
+  // var tl = new TimelineMax({repeat: -1, onUpdate:function() {
+  //   animation.moveTo(p);
+  // }});
+  // var e = Power2.easeIn;
+  //
+  //
+  // tl.to(p, 1, {x: 50, y: 0, z: 0, ease: e});
+  // tl.to(p, 1, {x: 50, y: 50, z: 0, ease: e});
+  // tl.to(p,1, {x: 0, y: 50, z: 0, ease: e});
+  // tl.to(p,1, {x: 0, y: 0, z: 0, ease: e});
+  //
+  // tl.timeScale(2);
 }
+
+
+
+
+function onDocumentMouseMove( event ) {
+
+  event.preventDefault();
+
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+  raycaster.setFromCamera( mouse, camera );
+
+  if ( SELECTED ) {
+
+    if ( raycaster.ray.intersectPlane( plane, intersection ) ) {
+
+      // TODO
+      // SELECTED.position.copy( intersection.sub( offset ) );
+
+      SELECTED.moveTo(intersection.sub( offset ));
+
+    }
+
+    return;
+
+  }
+
+  var intersects = raycaster.intersectObjects( objects );
+
+  if ( intersects.length > 0 ) {
+
+    if ( INTERSECTED != intersects[ 0 ].object ) {
+
+      // if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+
+      INTERSECTED = intersects[ 0 ].object;
+      // INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+
+      plane.setFromNormalAndCoplanarPoint(
+        camera.getWorldDirection( plane.normal ),
+        INTERSECTED.position );
+
+    }
+
+    container.style.cursor = 'pointer';
+
+  } else {
+
+    // if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+
+    INTERSECTED = null;
+
+    container.style.cursor = 'auto';
+
+  }
+
+}
+
+function onDocumentMouseDown( event ) {
+
+  event.preventDefault();
+
+  raycaster.setFromCamera( mouse, camera );
+
+  var intersects = raycaster.intersectObjects( objects );
+
+  if ( intersects.length > 0 ) {
+
+    controls.enabled = false;
+
+    SELECTED = intersects[ 0 ].object;
+
+    if ( raycaster.ray.intersectPlane( plane, intersection ) ) {
+
+      offset.copy( intersection ).sub( SELECTED.position );
+
+    }
+
+    container.style.cursor = 'move';
+
+  }
+
+}
+
+function onDocumentMouseUp( event ) {
+
+  event.preventDefault();
+
+  controls.enabled = true;
+
+  if ( INTERSECTED ) {
+
+    SELECTED = null;
+
+  }
+
+  container.style.cursor = 'auto';
+
+}
+
+
+
 
 ////////////////////
 // CLASSES
 ////////////////////
 
 function Animation() {
-  var model = new THREE.SphereGeometry(10, 32, 32);
+  var model = new THREE.SphereGeometry(10, 24, 24);
 
   //THREE.BAS.Utils.separateFaces(model);
 
-  var geometry = new THREE.BAS.ModelBufferGeometry(model, {
-    localizeFaces: false,
-    computeCentroids: false
-  });
-
-  // ANIMATION
-
-  // POSITION
-
-  //var aPosition = geometry.createAttribute('aPosition', 3, function(data, i) {
-  //  geometry.centroids[i].toArray(data);
-  //});
+  var geometry = new THREE.BAS.ModelBufferGeometry(model);
 
   geometry.createAttribute('aSmearFactor', 1, function(data) {
-    data[0] = Math.random() * 0.25;
+    data[0] = 1.0 + THREE.Math.randFloatSpread(0);
   });
 
-  var material = new THREE.BAS.BasicAnimationMaterial({
+  var material = new THREE.BAS.PhongAnimationMaterial({
     shading: THREE.FlatShading,
     side: THREE.DoubleSide,
-    wireframe: true,
+    // wireframe: true,
     uniforms: {
       uTime: {value: 0},
       uDelta: {value: new THREE.Vector3()}
     },
     uniformValues: {
-      diffuse: new THREE.Color(0x000000)
+      diffuse: new THREE.Color(0xffffff)
     },
     vertexFunctions: [
       THREE.BAS.ShaderChunk['ease_cubic_in_out'],
@@ -73,12 +192,13 @@ function Animation() {
     vertexInit: [
     ],
     vertexPosition: [
+      'vec3 nt = normalize(transformed);',
+      'vec3 nd = normalize(uDelta);',
+      'float dp = dot(nt, nd);',
 
-      'if (transformed.x < 0.0) {',
-        'transformed -= uDelta * aSmearFactor;',
+      'if (dp < 0.0) {',
+        'transformed -= uDelta * (1.0 - dp) * aSmearFactor;',
       '}'
-
-      //'transformed += aPosition;'
     ]
   });
 
@@ -100,15 +220,6 @@ Object.defineProperty(Animation.prototype, 'time', {
 
 Animation.prototype.moveTo = function(target) {
   this.material.uniforms['uDelta'].value.subVectors(target, this.position);
-
-  //this.positition.copy(target);
-  TweenMax.to(this.position, 10, {x:target.x, y:target.y, z:target.z, ease:Power0.easeIn});
-};
-
-
-Animation.prototype.animate = function(duration, options) {
-  options = options || {};
-  options.time = this.totalDuration;
-
-  return TweenMax.fromTo(this, duration, {time: 0.0}, options);
+  this.position.copy(target);
+  // TweenMax.to(this.position, 10, {x:target.x, y:target.y, z:target.z, ease:Power0.easeIn});
 };
