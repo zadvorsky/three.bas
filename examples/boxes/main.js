23796f643bac2029595801e90b96a42e155e75c4
Szenia Zadvornykh <s.zadvornykh@gmail.com>
2016-07-06 14:14:34 -0400

    v1

--- examples/boxes/main.js
+++ examples/boxes/main.js
@@ -12,6 +12,9 @@ function init() {
   var gridHelper = new THREE.GridHelper(10, 1);
   root.add(gridHelper);
 
+  var axisHelper = new THREE.AxisHelper(10);
+  root.add(axisHelper);
+
   var animation = new Animation();
   animation.animate(10.0, {repeat:-1, repeatDelay: 0.0, ease:Power0.easeNone});
   root.add(animation);
@@ -27,12 +30,12 @@ function Animation() {
 
   prefab.translate(0, prefabSize * 0.5, 0);
 
-  var gridSize = 2;
+  var gridSize = 1;
   var prefabCount = gridSize * gridSize;
 
   var geometry = new THREE.BAS.PrefabBufferGeometry(prefab, prefabCount);
 
-  var aPosition = geometry.createAttribute('aPosition', 3);
+  //var aPosition = geometry.createAttribute('aPosition', 3);
   var aDelayDuration = geometry.createAttribute('aDelayDuration', 2);
   var index = 0;
 
@@ -41,13 +44,12 @@ function Animation() {
 
   this.totalDuration = maxDelay + maxDuration;
 
-
   for (var i = 0; i < gridSize; i++) {
     for (var j = 0; j < gridSize; j++) {
       var x = THREE.Math.mapLinear(i, 0, gridSize, -gridSize * 0.5, gridSize * 0.5) + 0.5;
       var y = THREE.Math.mapLinear(j, 0, gridSize, -gridSize * 0.5, gridSize * 0.5) + 0.5;
 
-      geometry.setPrefabData(aPosition, index, [x, 0, y]);
+      //geometry.setPrefabData(aPosition, index, [x, 0, y]);
 
       //var delay = maxDelay * index / prefabCount;
       var delay = maxDelay * Math.sqrt(x * x + y * y) / gridSize * 0.5;
@@ -55,78 +57,85 @@ function Animation() {
 
       var duration = maxDuration;
 
-      geometry.setPrefabData(aDelayDuration, index, [delay, duration]);
+      geometry.setPrefabData(aDelayDuration, index, [0, 2]);
 
       index++;
     }
   }
 
+  /**
+   float cDelay{{step}} = x;
+   float cDuration{{step}} = x;
+   vec3 cTranslate{{step}} = vec(x, x, x);
+   vec3 cScale{{step}} = vec(x, x, x);
+   void applyTransform{{step}}(inout vec3 v) {
+     if (uTime < cDelay{{step}}) return;
+
+     float progress = {{ease}}((uTime - cDelay{{step}}) / cDuration{{step}});
+
+     v *= cScale{{step}} * progress;
+     v += cTranslate{{step}} * progress;
+    }
+   */
+
+  function vecToString(v, p) {
+    return v.x.toPrecision(p) + ',' + v.y.toPrecision(p) + ',' + v.z.toPrecision(p);
+  }
+
+  function makeShaderChunk(key, delay, duration, ease, translate, scale) {
+    var t = 'vec3(' + vecToString(translate, 2) + ')';
+    var s = 'vec3(' + vecToString(scale, 2) + ')';
+
+    return [
+      'float cDelay' + key + ' = ' + delay.toPrecision(2) + ';',
+      'float cDuration' + key + ' = ' + duration.toPrecision(2) + ';',
+      'vec3 cTranslate' + key + ' = ' + t + ';',
+      'vec3 cScale' + key + ' = ' + s + ';',
+
+      'vec3 applyTransform' + key + '(float time, vec3 v) {',
+
+        //'if (time < cDelay' + key + ') return;',
+
+        'float progress = clamp(time - cDelay' + key + ', 0.0, cDuration' + key + ') / cDuration' + key + ';',
+        'progress = ' + ease + '(progress);',
+
+        'v *= cScale' + key + ' * progress;',
+        'v += cTranslate' + key + ' * progress;',
+
+        'return v;',
+      '}'
+    ].join('\n');
+  }
 
 
-
-  // ANIMATION FRAME DATA -> [scl.x, scl.y, scl.z, pos.y]
-
-  var animationFrames = [];
-
-  animationFrames.push(new THREE.Vector4(1.0, 1.0, 1.0, 0.0));
-  animationFrames.push(new THREE.Vector4(1.5, 0.5, 1.5, 0.0));
-  animationFrames.push(new THREE.Vector4(0.5, 2.0, 0.5, 6.0));
-  animationFrames.push(new THREE.Vector4(1.0, 1.0, 1.0, 0.0));
-  animationFrames.push(new THREE.Vector4(1.5, 0.2, 1.5, 0.0));
-  animationFrames.push(new THREE.Vector4(1.0, 1.0, 1.0, 0.0));
-
-
-
-
-  //animationFrames.push(new THREE.Vector4(1.0, 1.0, 1.0, 0.0));
-  //animationFrames.push(new THREE.Vector4(1.125, 0.75, 1.125, 0.0));
-  //animationFrames.push(new THREE.Vector4(0.5, 2.0, 0.5, 4.0));
-  //animationFrames.push(new THREE.Vector4(0.5, 2.0, 0.5, 0.0));
-  //animationFrames.push(new THREE.Vector4(1.5, 0.5, 1.5, 0.0));
-  //animationFrames.push(new THREE.Vector4(1.0, 1.0, 1.0, 0.0));
+  var animationStep01 = makeShaderChunk('01', 0.0, 1.0, 'easeCubicInOut', new THREE.Vector3(0, 10, 0), new THREE.Vector3(0, 2, 0));
+  var animationStep02 = makeShaderChunk('02', 1.0, 1.0, 'easeCubicInOut', new THREE.Vector3(0, -10, 0), new THREE.Vector3(0, -2, 0));
 
 
   var material = new THREE.BAS.StandardAnimationMaterial({
     shading: THREE.FlatShading,
     uniforms: {
-      uTime: {value: 0},
-      uFrames: {value: animationFrames},
-    },
-    defines: {
-      FRAME_COUNT: animationFrames.length,
-      FRAME_MAX: (animationFrames.length - 1).toFixed(1)
+      uTime: {value: 0}
     },
     uniformValues: {
     },
     vertexFunctions: [
-      THREE.BAS.ShaderChunk['catmull_rom_spline'],
-      THREE.BAS.ShaderChunk['ease_bezier']
+      THREE.BAS.ShaderChunk['ease_cubic_in_out'],
+
+      animationStep01,
+      animationStep02
     ],
     vertexParameters: [
-      'uniform vec4 uFrames[FRAME_COUNT];',
       'uniform float uTime;',
 
-      'attribute vec3 aPosition;',
+      //'attribute vec3 aPosition;',
       'attribute vec2 aDelayDuration;'
     ],
     vertexPosition: [
-      'float tProgress = clamp(uTime - aDelayDuration.x, 0.0, aDelayDuration.y) / aDelayDuration.y;',
-      'tProgress = easeBezier(tProgress, vec4(.5,.12,.87,.49));',
+      'float tTime = clamp(uTime - aDelayDuration.x, 0.0, aDelayDuration.y);',
 
-      'float animationProgress = tProgress * FRAME_MAX;',
-      'ivec4 indices = getCatmullRomSplineIndices(FRAME_MAX, animationProgress);',
-
-      'vec4 p0 = uFrames[indices[0]];',
-      'vec4 p1 = uFrames[indices[1]];',
-      'vec4 p2 = uFrames[indices[2]];',
-      'vec4 p3 = uFrames[indices[3]];',
-
-      'float animationProgressFract = fract(animationProgress);',
-
-      'transformed *= catmullRomSpline(p0.xyz, p1.xyz, p2.xyz, p3.xyz, animationProgressFract, vec2(0));',
-
-      'transformed += aPosition;',
-      'transformed.y += catmullRomSpline(p0.w, p1.w, p2.w, p3.w, animationProgressFract, vec2(0));',
+      'transformed += applyTransform01(tTime, position);',
+      'transformed += applyTransform02(tTime, position);'
 
     ]
   });
