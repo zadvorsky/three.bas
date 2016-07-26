480350e895c8627ad3a196e11e88e9818ae1a32c
Szenia Zadvornykh <s.zadvornykh@gmail.com>
2016-07-26 12:32:28 -0400

    cleaned up speed particle example

--- examples/cavalier_speed_particles/main.js
+++ examples/cavalier_speed_particles/main.js
@@ -1,22 +1,67 @@
 window.onload = init;
 
 function init() {
-  var root = new THREERoot();
+  var root = new THREERoot({
+    zNear: 0.1,
+    zFar: 20000
+  });
   root.renderer.setClearColor(0x000000);
-  root.camera.position.set(0, 2, 8);
-  
+  root.camera.position.set(0, 0.1, 1.0).multiplyScalar(20);
+
+  // settings for the size of the animation / speed particle system
   var width = 20;
   var height = 10;
-  var depth = 20;
-  
-  var grid = new THREE.GridHelper(width * 0.5, 1);
-  root.add(grid);
-  
-  var animation = new Animation(width, height, depth, 10000, 0.01);
-  root.add(animation);
+  var depth = 40;
+
+  // create a ground for reference
+  var ground = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(width, depth, width - 1, depth - 1),
+    new THREE.MeshBasicMaterial({
+      wireframe: true,
+      color: 0x222222
+    })
+  );
+  ground.rotateX(-Math.PI * 0.5);
+  root.add(ground);
+
+  // animation
+  var animation;
+
+  // gui
+  var gui = new dat.GUI();
+  var controller = {
+    speedScale: 100.0,
+    timeStep: (1/60),
+    count: 10000,
+    size: 0.01,
+    create: function() {
+      if (animation) {
+        root.remove(animation);
+        animation.geometry.dispose();
+        animation.material.dispose();
+      }
+
+      animation = new Animation(width, height, depth, controller.count, controller.size);
+      animation.setScale(controller.speedScale);
+      root.add(animation);
+    }
+  };
+
+  gui.add(controller, 'timeStep', (1/300), (1/60));
+  gui.add(controller, 'speedScale', 1.0, 300.0).onChange(function(v) {
+    animation.setScale(v);
+    root.camera.fov = 60 + v * 0.20;
+    root.camera.updateProjectionMatrix();
+  });
+  gui.add(controller, 'count', 100, 500000).step(100);
+  gui.add(controller, 'size', 0.001, 0.1).step(0.001);
+  gui.add(controller, 'create').name('> update');
+
+  controller.create();
+
   root.addUpdateCallback(function() {
-    animation.update(1/60);
-  })
+    animation && animation.update(controller.timeStep);
+  });
 }
 
 ////////////////////
@@ -24,56 +69,55 @@ function init() {
 ////////////////////
 
 function Animation(width, height, depth, prefabCount, prefabSize) {
+  // create a prefab
   var prefab = new THREE.TetrahedronGeometry(prefabSize);
+
+  // create a geometry where the prefab is repeated 'prefabCount' times
   var geometry = new SpeedParticleGeometry(prefab, prefabCount);
-  
+
+  // add a time offset for each prefab, spreading them out along the Z axis
   geometry.createAttribute('aOffset', 1, function(data, i, count) {
     data[0] = i / count;
   });
-  
+
+  // create a start position for each prefab
   var aStartPosition = geometry.createAttribute('aStartPosition', 3);
+  // create an end position for each prefab
   var aEndPosition = geometry.createAttribute('aEndPosition', 3);
   var x, y, data = [];
-  
+
+  // for each prefab
   for (var i = 0; i < prefabCount; i++) {
+    // get a random x coordinate between -width/2 and width/2
     x = THREE.Math.randFloatSpread(width);
+    // get a random y coordinate between 0 and height
     y = THREE.Math.randFloat(0, height);
-    
+
+    // store the coordinates in the buffer attribute
+    // x and y are the same for start and end position, causing each prefab to move in a straight line
     data[0] = x;
     data[1] = y;
+    // all prefabs start at depth * -0.5
     data[2] = depth  * -0.5;
     geometry.setPrefabData(aStartPosition, i, data);
     
     data[0] = x;
     data[1] = y;
+    // all prefabs end at depth * 0.5
     data[2] = depth * 0.5;
     geometry.setPrefabData(aEndPosition, i, data);
   }
-  
-  var axis = new THREE.Vector3();
-  
-  geometry.createAttribute('aAxisAngle', 4, function(data) {
-    THREE.BAS.Utils.randomAxis(axis);
-    
-    axis.toArray(data);
-    data[3] = Math.PI * THREE.Math.randInt(8, 16);
-  });
-  
+
   var material = new THREE.BAS.BasicAnimationMaterial({
-    shading: THREE.FlatShading,
     side: THREE.DoubleSide,
     uniforms: {
       uTime: {value: 0.0},
       uDuration: {value: 1.0},
-      uScale: {value: 80.0}
+      uScale: {value: 1.0}
     },
     uniformValues: {
       diffuse: new THREE.Color(0xf1f1f1)
     },
-    vertexFunctions: [
-      THREE.BAS.ShaderChunk['quaternion_rotation'],
-      THREE.BAS.ShaderChunk['cubic_bezier']
-    ],
     vertexParameters: [
       'uniform float uTime;',
       'uniform float uDuration;',
@@ -81,17 +125,14 @@ function Animation(width, height, depth, prefabCount, prefabSize) {
   
       'attribute float aOffset;',
       'attribute vec3 aStartPosition;',
-      'attribute vec3 aEndPosition;',
-      'attribute vec4 aAxisAngle;'
+      'attribute vec3 aEndPosition;'
     ],
     vertexPosition: [
+      // calculate a time based on the uniform time and the offset of each prefab
       'float tProgress = mod((uTime + aOffset), uDuration) / uDuration;',
-  
-      'float angle = aAxisAngle.w * tProgress;',
-      'vec4 tQuat = quatFromAxisAngle(aAxisAngle.xyz, angle);',
-      
-      'transformed = rotateVector(tQuat, transformed);',
+      // scale the z axis based on the uniform speed scale
       'transformed.z *= uScale;',
+      // translate between start and end position based on progress
       'transformed += mix(aStartPosition, aEndPosition, tProgress);'
     ]
   });
@@ -105,9 +146,8 @@ Animation.prototype.constructor = Animation;
 Animation.prototype.update = function(delta) {
   this.material.uniforms['uTime'].value += delta;
 };
-Animation.prototype.setScaleStep = function(scale) {
-  scale = Math.max(1.0, scale * 128);
-  TweenMax.to(this.material.uniforms['uScale'], 1.0, {value:scale, ease:Power0.easeIn});
+Animation.prototype.setScale = function(scale) {
+  this.material.uniforms['uScale'].value = scale;
 };
 
 function SpeedParticleGeometry(prefab, count) {
@@ -115,31 +155,37 @@ function SpeedParticleGeometry(prefab, count) {
 }
 SpeedParticleGeometry.prototype = Object.create(THREE.BAS.PrefabBufferGeometry.prototype);
 SpeedParticleGeometry.prototype.constructor = SpeedParticleGeometry;
+
+// override THREE.BAS.PrefabBufferGeometry.bufferPosition
+// instead of simply copying the prefab, a random scale and rotation is applied
 SpeedParticleGeometry.prototype.bufferPositions = function() {
   var positionBuffer = this.createAttribute('position', 3).array;
-  
+
   var axis = new THREE.Vector3();
   var scaleMatrix = new THREE.Matrix4();
   var rotationMatrix = new THREE.Matrix4();
   var transformMatrix = new THREE.Matrix4();
   var p = new THREE.Vector3();
-  
+
+  // for each prefab, compute a random transformation
   for (var i = 0, offset = 0; i < this.prefabCount; i++) {
+    // random scale
     scaleMatrix.identity().makeScale(Math.random(), Math.random(), Math.random());
-    
-    axis.set(Math.random(), Math.random(), Math.random());
-    axis.normalize();
-    
+
+    // random axis rotation
+    THREE.BAS.Utils.randomAxis(axis);
     rotationMatrix.identity().makeRotationAxis(axis, Math.random() * Math.PI * 2);
-    
+
+    // mush the two matrices together
     transformMatrix.multiplyMatrices(scaleMatrix, rotationMatrix);
-    
+
+    // for each prefab vertex, apply the transformation matrix
     for (var j = 0; j < this.prefabVertexCount; j++, offset += 3) {
       var prefabVertex = this.prefabGeometry.vertices[j];
-      
+
       p.copy(prefabVertex);
       p.applyMatrix4(transformMatrix);
-      
+
       positionBuffer[offset    ] = p.x;
       positionBuffer[offset + 1] = p.y;
       positionBuffer[offset + 2] = p.z;
